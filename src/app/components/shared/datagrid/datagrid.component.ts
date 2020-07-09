3b63c39ee6bef63e5cb8a072508f0b90adb11f13
Sixto José Romero Martínez <sixto.jose@gmail.com>
2020-07-09 17:28:56 -0500

    Se aplican cambios

--- src/app/components/shared/datagrid/datagrid.component.ts
+++ src/app/components/shared/datagrid/datagrid.component.ts
@@ -9,15 +9,7 @@ import { EmployeesModel } from 'src/app/models/employees.model';
 })
 export class DatagridComponent implements OnInit {
 
-  columnDefs = [
-    {headerName: 'Id', field: 'id', sortable: true, filter: true },
-    {headerName: 'Name', field: 'name', sortable: true, filter: true },
-    {headerName: 'Contract TypeName', field: 'contractTypeName', sortable: true, filter: true},
-    {headerName: 'Hourly Salary', field: 'hourlySalary', sortable: true, filter: true},
-    {headerName: 'Monthly Salary', field: 'monthlySalary', sortable: true, filter: true},
-    {headerName: 'Annual Salary', field: 'annualSalary', sortable: true, filter: true}
-  ];
-
+  
   @Input() employees: EmployeesModel[] = [];
   iemployees = new EmployeesModel();
 
